6a7178d158722eb16eab64af55713dea41884654
JP Hastings-Spital <jphastings@blinkbox.com>
2014-08-14 17:44:16 +0100

    Allow adding of service version to default params

--- lib/blinkbox/common_logging.rb
+++ lib/blinkbox/common_logging.rb
@@ -2,8 +2,14 @@ require "gelf"
 
 module Blinkbox
   class CommonLogging < GELF::Logger
-    def initialize(host: "localhost", port: 12201, facility: $0, max_size: 8192)
-      super(host, port, max_size, { facility: facility })
+    def initialize(host: "localhost", port: 12201, facility: $0, max_size: 8192, service_version: nil)
+      options = { facility: facility }
+      options[:serviceVersion] = service_version unless service_version.nil?
+      super(host, port, max_size, options)
+    end
+
+    def service_version=(semver)
+      self.default_options['serviceVersion'] = semver
     end
 
     def self.from_config(hash)
