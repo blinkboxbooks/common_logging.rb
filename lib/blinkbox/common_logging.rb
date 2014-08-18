b8d129a6676d66d36b84e72b587f751e55ba4811
JP Hastings-Spital <jphastings@blinkbox.com>
2014-08-18 11:04:02 +0100

    More comments

--- lib/blinkbox/common_logging.rb
+++ lib/blinkbox/common_logging.rb
@@ -8,18 +8,34 @@ module Blinkbox
       super(host, port, max_size, options)
     end
 
+    # Allows the setting of the facility version default parameter.
+    # @param [String] semver The semantic version of the service which is sending logs
     def facility_version=(semver)
       self.default_options['facilityVersion'] = semver
     end
 
+    # Accepts a hash detailing log config settings in the format used in blinkbox Books
+    # config files and returns a CommonLogging object.
+    #
+    # @example Load from config
+    #   config = Blinkbox::CommonConfig.new
+    #   logger = Blinkbox::CommonLogging.from_config(config.tree(:logging))
+    #
+    # @param [Hash] hash The returned hash from CommonConfig#tree
+    # @option hash [String] :udp.host (localhost) The Graylog host
+    # @option hash [Integer] :udp.port (12201) The Graylog port number
+    # @option hash [String] :gelf.facility Organisation/app_name eg. Marvin/cover_processor
+    # @option hash [Integer] :gelf.maxChunkSize (8192) The default size for UDP chunks sent to the server.
+    # @option hash ["DEBUG","INFO","WARN","ERROR","FATAL"] :level ("INFO") The level below which messages will not be sent to Graylog.
+    # @return [CommonLogging]
     def self.from_config(hash)
       logger = new(
-        host: hash["udp"]["host"],
-        port: hash["udp"]["port"],
-        facility: hash["gelf"]["facility"],
-        max_size: hash["gelf"]['maxChunkSize']
+        host: hash[:udp][:host],
+        port: hash[:udp][:port],
+        facility: hash[:gelf][:facility],
+        max_size: hash[:gelf][:maxChunkSize] || 8192
       )
-      logger.level = GELF.const_get(hash["level"].upcase)
+      logger.level = GELF.const_get(hash[:level].upcase)
       logger
     end
   end
